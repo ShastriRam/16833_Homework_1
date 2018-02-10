b885a0dfeba0e37ed0e0155401598b09515adc0e
Rahul Ramakrishnan <rahulramkrish23@gmail.com>
2018-02-10 12:41:53 -0500

    Resampling file completed

--- code/scripts/Resampling.py
+++ code/scripts/Resampling.py
@@ -12,6 +12,8 @@ class Resampling:
         """
         TODO : Initialize resampling process parameters here
         """
+        M = 500
+        X_bar_resampled = []
 
     def multinomial_sampler(self, X_bar):
 
@@ -23,6 +25,12 @@ class Resampling:
         """
         TODO : Add your code here
         """
+        p = X_bar[:,4]
+        p = p/numpy.sum(p) #p = np.divide(p, numpy.sum(p))
+        if sum(p) != 1.: raise ValueError, "p must sum to 1"
+        uniform_sample = np.random.uniform(size=M)
+        p_cum = np.cumsum(p)
+        X_bar_resampled = np.searchsorted(p_cum,uniform_sample,side='right')
 
         return X_bar_resampled
 
@@ -36,7 +44,18 @@ class Resampling:
         """
         TODO : Add your code here
         """
-        
+        r = np.random.uniform(0,1/M)
+        c = X_bar[0,4]
+        print(c)
+        i = 1
+        for m in range(1,M)
+            U = r + (m − 1) * (1/M)
+            while U > c :
+                i = i + 1
+                c = c + X_bar[i,4]
+            X_bar_resampled = np.append(X_bar[i],axes = 0)
+        X_bar_resampled = np.asarray(X_bar_resampled)
+
         return X_bar_resampled
 
 if __name__ == "__main__":
